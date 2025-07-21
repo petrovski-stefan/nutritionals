ed01d19befdf0bb2c9dde42f10efb83e8e5768d1
Stefan Petrovski <spetrovski09@gmail.com>
2025-07-21 14:01:49 +0200

    Add products model admin

--- backend/src/products/admin.py
+++ backend/src/products/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Product
 
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "pharmacy"]
+    readonly_fields = ["created_at", "updated_at"]
