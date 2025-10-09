7e9cd8a645a7388be6c3d2188bd65a609c077afd
Stefan Petrovski <spetrovski09@gmail.com>
2025-10-09 13:46:19 +0200

    Config axios to throw exception only on 5xx

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL as string;
 
-const axiosInstance = axios.create({ baseURL: baseUrl });
+const axiosInstance = axios.create({
+  baseURL: baseUrl,
+  validateStatus: (status) => status >= 200 && status < 500,
+});
 
 export default axiosInstance;
