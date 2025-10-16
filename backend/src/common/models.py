1971561ee661a12919c90a2f6ee614a4ac6d62be
Stefan Petrovski <spetrovski09@gmail.com>
2025-10-16 13:25:15 +0200

    Add a base timestamped abstract model and delete useless modules in common app

--- backend/src/common/models.py
+++ backend/src/common/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    """
+    Abstract model which provides created_at and updated_at timestamps
+    """
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
