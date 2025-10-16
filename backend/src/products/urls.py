76a9ff98d8c398220fe65174bdc59b903e1e1ad2
Stefan Petrovski <spetrovski09@gmail.com>
2025-10-16 13:24:04 +0200

    Update product url names and fix formatting

--- backend/src/products/urls.py
+++ backend/src/products/urls.py
@@ -3,8 +3,14 @@ from django.urls import path
 from .views import ProductDeprecatedBrandListAPIView, ProductDeprecatedListAPIView
 
 urlpatterns = [
-    path("", view=ProductDeprecatedListAPIView.as_view(), name="product-list"),
     path(
-        "brands/", view=ProductDeprecatedBrandListAPIView.as_view(), name="product-list"
+        "",
+        view=ProductDeprecatedListAPIView.as_view(),
+        name="product-list",
+    ),
+    path(
+        "brands/",
+        view=ProductDeprecatedBrandListAPIView.as_view(),
+        name="product-brands-list",
     ),
 ]
