b29aaeded7f23baceae296e4d775990418a53a8b
Stefan Petrovski <spetrovski09@gmail.com>
2025-10-16 13:30:01 +0200

    Add celery timezone setting

--- backend/src/core/settings/celery.py
+++ backend/src/core/settings/celery.py
@@ -6,3 +6,4 @@ CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
 CELERY_BROKER_URL = env("CELERY_BROKER_URL")
 # CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND")
+CELERY_TIMEZONE = "Europe/Berlin"
