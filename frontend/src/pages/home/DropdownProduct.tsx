074726e84a6a994e2f26f09ceac451d0cd937b84
Stefan Petrovski <spetrovski09@gmail.com>
2025-11-18 10:26:36 +0100

    Standardize home page dropdown products

--- frontend/src/pages/home/DropdownProduct.tsx
+++ frontend/src/pages/home/DropdownProduct.tsx
@@ -3,12 +3,23 @@ type Props = {
   price: string;
   discountPrice: string;
   pharmacyLogo: string;
+  url: string;
 };
 
-export default function DropdownProductCard({ name, price, discountPrice, pharmacyLogo }: Props) {
+export default function DropdownProductCard({
+  name,
+  price,
+  discountPrice,
+  pharmacyLogo,
+  url,
+}: Props) {
   const hasDiscountPrice = discountPrice !== '';
-  const priceStyles = hasDiscountPrice ? 'line-through text-dark/50' : 'text-dark';
-  const discountPriceStyles = hasDiscountPrice ? 'text-primary font-semibold ml-2' : '';
+  const originalPriceStyles = hasDiscountPrice
+    ? 'line-through text-accent text-sm text-primary'
+    : 'text-xl font-semibold text-primary';
+  const discountPriceStyles = hasDiscountPrice
+    ? 'text-xl font-semibold ml-2 text-accent'
+    : 'hidden';
 
   return (
     <div className="flex items-center justify-between gap-4 rounded-lg px-4 py-2 transition-colors hover:bg-gray-500/20">
@@ -22,12 +33,18 @@ export default function DropdownProductCard({ name, price, discountPrice, pharma
       </div>
 
       {/* Product Name */}
-      <p className="text-dark flex-1 truncate">{name}</p>
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <p className="text-dark flex-1 truncate">{name}</p>
+      </a>
 
       {/* Prices */}
       <div className="flex items-center gap-1">
-        <span className={priceStyles}>{price}</span>
-        {hasDiscountPrice && <span className={discountPriceStyles}>{discountPrice}</span>}
+        <span className={originalPriceStyles}>{price}</span>
+        <span className={discountPriceStyles}>{discountPrice}</span>
       </div>
     </div>
   );
