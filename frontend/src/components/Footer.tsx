f808cc3a1a0cde912ced171802dc0031a339bce7
Stefan Petrovski <spetrovski09@gmail.com>
2025-11-21 15:59:05 +0100

    Implement locale in footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,25 +1,18 @@
-const EMAIL_CONTACT = import.meta.env.VITE_NUTRITIONALS_EMAIL as string;
+import { Link } from 'react-router-dom';
+import FOOTER_TEXT from '../locale/footer';
 
 export default function Footer() {
   return (
     <footer className="bg-dark text-neutral flex h-[8vh] items-center">
       <p className="w-full text-center text-sm">
-        This site is a bachelor's thesis project for educational purposes. No products are sold
-        here. For legal inquiries or data removal requests, contact{' '}
-        <a
-          href={`mailto:${EMAIL_CONTACT}`}
-          className="text-accent hover:text-accent/90 underline"
+        {FOOTER_TEXT['p']}
+        <Link
+          className="text-accent hover:decoration-accent hover:underline"
+          to={'/about'}
         >
-          {EMAIL_CONTACT}
-        </a>
-        . See our{' '}
-        <a
-          href="/about"
-          className="text-accent hover:text-accent/90 underline"
-        >
-          About & Legal
-        </a>{' '}
-        page for full disclaimer.
+          {FOOTER_TEXT['about']}
+        </Link>
+        {FOOTER_TEXT['dot']}
       </p>
     </footer>
   );
