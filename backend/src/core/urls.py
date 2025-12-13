65470d792140cfafe3f9ca3c969f10671ec2e746
Stefan Petrovski <spetrovski09@gmail.com>
2025-12-13 13:10:23 +0100

    Update django admin site title

--- backend/src/core/urls.py
+++ backend/src/core/urls.py
@@ -2,6 +2,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+# Admin site config
+admin.site.site_header = "NUTRITIONALS"
+admin.site.site_title = "NUTRITIONALS"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/products/", include("products.urls")),
